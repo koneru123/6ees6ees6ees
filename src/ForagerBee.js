dbdabcf307cc2411f983e1430869b33a9511c503
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-03-30 22:00:00 -0700

    es6 implementation

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
-class ForagerBee {
-  // TODO..
-};
+class ForagerBee extends Bee {
+  constructor(age, job, color, food, eat, canFly, treasureChest, forage) {
+    super(age, color, food, eat, job);
+    this.age = 10,
+    this.job = 'find pollen',
+    this.canFly = true,
+    this.treasureChest = []
+  }
+  forage() {
+    this.treasureChest.push('treasure');
+  }
+}
